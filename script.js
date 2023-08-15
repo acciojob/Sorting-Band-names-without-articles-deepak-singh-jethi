e57afee63cea3c3390eb0f829350f31dcd82638a
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-08-15 23:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here
 
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp ={};
+let arr2 = [];
+
+for(let i = 0; i<arr.length; i++){
+	
+    let words = sentence.split(" ");
+	let finalString ="";
+	
+	for(let j = 0; j<words.length; j++){
+		let currentWord = words[j].toLowerCase();
+		if(currentWord!="an" && currentWord!="the" && currentWord!="a" ){
+			finalString+=word[j]+" ";
+			
+		}
+	}
+	let articleLessString = finalString.trim();
+	mp[articleLessString] = arr[i];
+	arr2.push(articleLessString);
+}
+
+arr2.sort();
+
+for(let i =0; i<arr2.length; i++){
+	arr[i] = mp[arr2[i]];
+}
+console.log(arr);
